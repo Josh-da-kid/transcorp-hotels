8dbc58bc68e9500f26e3dbd3c55c923c3c990155
Josiah <carmenjosh84@gmail.com>
2026-01-27 15:19:15 +0100

    updated

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -17,7 +17,7 @@
 	</div>
 </div>
 
-<section class="flex flex-col gap-3 py-12 text-center">
+<section class="flex flex-col gap-3 px-4 py-12 text-center">
 	<p class="cinzel text-amber-300">Welcome</p>
 	<h2 class="cinzel text-2xl lg:text-4xl">
 		EXPERIENCE <br /> CALABAR'S PREMIER <br class="hidden sm:hidden" /> DESTINATION
@@ -27,20 +27,40 @@
 		World class service, elegant rooms, exceptional <br /> dining.
 	</p>
 
-	<section class="py-8 px-4">
-		<div class="flex max-w-sm items-center justify-between mx-auto">
-			<div class="flex flex-col justify-center items-center">
-                <svg class="text-amber-300" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="M21 10.78V8c0-1.65-1.35-3-3-3h-4c-.77 0-1.47.3-2 .78c-.53-.48-1.23-.78-2-.78H6C4.35 5 3 6.35 3 8v2.78c-.61.55-1 1.34-1 2.22v6h2v-2h16v2h2v-6c0-.88-.39-1.67-1-2.22M14 7h4c.55 0 1 .45 1 1v2h-6V8c0-.55.45-1 1-1M5 8c0-.55.45-1 1-1h4c.55 0 1 .45 1 1v2H5zm-1 7v-2c0-.55.45-1 1-1h14c.55 0 1 .45 1 1v2z"/></svg>
-                <p class=" cinzel-bold text-2xl">130+</p>
-                <p class="text-gray-400">Luxury Rooms</p>
-            </div>
+	<section class="px-4 py-8">
+		<div class="mx-auto flex max-w-sm items-center justify-between">
+			<div class="flex flex-col items-center justify-center">
+				<svg
+					class="text-amber-300"
+					xmlns="http://www.w3.org/2000/svg"
+					width="32"
+					height="32"
+					viewBox="0 0 24 24"
+					><path
+						fill="currentColor"
+						d="M21 10.78V8c0-1.65-1.35-3-3-3h-4c-.77 0-1.47.3-2 .78c-.53-.48-1.23-.78-2-.78H6C4.35 5 3 6.35 3 8v2.78c-.61.55-1 1.34-1 2.22v6h2v-2h16v2h2v-6c0-.88-.39-1.67-1-2.22M14 7h4c.55 0 1 .45 1 1v2h-6V8c0-.55.45-1 1-1M5 8c0-.55.45-1 1-1h4c.55 0 1 .45 1 1v2H5zm-1 7v-2c0-.55.45-1 1-1h14c.55 0 1 .45 1 1v2z"
+					/></svg
+				>
+				<p class=" cinzel-bold text-2xl">130+</p>
+				<p class="text-gray-400">Luxury Rooms</p>
+			</div>
 			<div class="h-11 w-[3px] bg-amber-300"></div>
 
-				<div class="flex flex-col justify-center items-center">
-                <svg class="text-amber-300" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="M21 10.78V8c0-1.65-1.35-3-3-3h-4c-.77 0-1.47.3-2 .78c-.53-.48-1.23-.78-2-.78H6C4.35 5 3 6.35 3 8v2.78c-.61.55-1 1.34-1 2.22v6h2v-2h16v2h2v-6c0-.88-.39-1.67-1-2.22M14 7h4c.55 0 1 .45 1 1v2h-6V8c0-.55.45-1 1-1M5 8c0-.55.45-1 1-1h4c.55 0 1 .45 1 1v2H5zm-1 7v-2c0-.55.45-1 1-1h14c.55 0 1 .45 1 1v2z"/></svg>
-                <p class=" cinzel-bold text-2xl">130+</p>
-                <p class="text-gray-400">Luxury Rooms</p>
-            </div>
+			<div class="flex flex-col items-center justify-center">
+				<svg
+					class="text-amber-300"
+					xmlns="http://www.w3.org/2000/svg"
+					width="32"
+					height="32"
+					viewBox="0 0 24 24"
+					><path
+						fill="currentColor"
+						d="M21 10.78V8c0-1.65-1.35-3-3-3h-4c-.77 0-1.47.3-2 .78c-.53-.48-1.23-.78-2-.78H6C4.35 5 3 6.35 3 8v2.78c-.61.55-1 1.34-1 2.22v6h2v-2h16v2h2v-6c0-.88-.39-1.67-1-2.22M14 7h4c.55 0 1 .45 1 1v2h-6V8c0-.55.45-1 1-1M5 8c0-.55.45-1 1-1h4c.55 0 1 .45 1 1v2H5zm-1 7v-2c0-.55.45-1 1-1h14c.55 0 1 .45 1 1v2z"
+					/></svg
+				>
+				<p class=" cinzel-bold text-2xl">130+</p>
+				<p class="text-gray-400">Luxury Rooms</p>
+			</div>
 		</div>
 	</section>
 </section>
